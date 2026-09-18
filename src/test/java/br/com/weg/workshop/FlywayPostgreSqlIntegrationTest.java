954831738300424c766c35f4d8bef57f8d784c12
JUNIOR GABRIEL ANTUNES DE SOUZA <junior_ga_souza@estudante.sesisenai.org.br>
2026-09-18 09:07:36 -0300

    test(foundation): cover foundation contracts [TASK-006]

---
+++ src/test/java/br/com/weg/workshop/FlywayPostgreSqlIntegrationTest.java
@@ -0,0 +1,30 @@
+package br.com.weg.workshop;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers(disabledWithoutDocker = true)
+@SpringBootTest
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
+class FlywayPostgreSqlIntegrationTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    void flywayAppliesInitialMigration() {
+        Integer appliedMigrations = jdbcTemplate.queryForObject(
+                "select count(*) from workshop.flyway_schema_history where version = '1' and success = true",
+                Integer.class);
+
+        assertThat(appliedMigrations).isEqualTo(1);
+    }
+}
